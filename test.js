98d281dfbfa4d22461bc403226e81ba6a67848a6
Damien Lecoq <damienlecoq1@gmail.com>
2024-10-02 17:13:01 +0200

    Commit rename files, sort images and fonts

--- test.js
+++
@@ -1,6 +0,0 @@
-// Exemples pour ajouter une animation de mouvement ou de changement de couleur.
-const lights = document.querySelectorAll('.light');
-
-lights.forEach((light, index) => {
-    light.style.animationDelay = `${index * 0.5}s`;
-});
