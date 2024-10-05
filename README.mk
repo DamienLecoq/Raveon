1513b7b9760dae45a2ef5e62aa26f19da7e3ab99
Damien Lecoq <damienlecoq1@gmail.com>
2024-10-05 09:40:49 +0200

    Readme and new lightsOffText anim

---
+++ README.mk
@@ -0,0 +1,13 @@
+# Raveon - Official Website
+
+## Description
+**Raveon** is a DJ and techno music producer specializing in rave culture. This website is a digital showcase where fans and promoters can easily access Raveon’s latest releases, upcoming events, and contact information. Built on a voluntary basis, this platform serves as the go-to place to stay connected with the artist. You can visit the website by simply typing [**itsraveon.com**](http://itsraveon.com) into your browser.
+
+## Features
+- **Contact Information:** Quickly find all ways to contact Raveon for bookings or inquiries.
+- **Social Media Links:** Follow Raveon on popular social platforms to stay up to date.
+- **Latest Releases:** Discover Raveon’s newest tracks and projects.
+- **Upcoming Events:** Get details on the next rave or event Raveon will be performing at.
+
+## Usage
+To visit the live site, simply go to [**itsraveon.com**](http://itsraveon.com) using any web browser.
